73cc4ed0ff295f5de337800cab61649bc60f1079
syadk <41011700+syadk@users.noreply.github.com>
2018-11-24 23:32:58 -0800

    initial upload

---
+++ 3Pts/3Pts Dataframes/produces indicator dataframe for 3pts.py
@@ -0,0 +1,111 @@
+import pandas as pd
+from datetime import datetime, timedelta
+import numpy as np
+import os 
+
+
+N=5
+
+def partTwo(GameID,df):
+    N2=1
+    threshold=0.5
+    df_Game = df.loc[df['GameID'] == GameID]
+    oppTeam=df_Game['OPP TEAM'].iloc[0]
+    day=df_Game['DATE'].iloc[0]
+    place=df_Game['VENUE (R/H)'].iloc[0]
+    df_out=pd.DataFrame()        
+
+    
+	################################################################################
+	#Opposing team data
+    df_Opp_Ag=df[df['OPP TEAM']==oppTeam]
+    
+	#What was the date of the opposing teams last N
+    oppDates=(df_Opp_Ag[df_Opp_Ag['DATE']<day])['DATE'].unique()
+    if len(oppDates)-N>=0:
+        dayNback=oppDates[len(oppDates)-N]
+    
+    	  #Only want data for the opposing teams previous N games
+        df_Opp_Ag=df_Opp_Ag[(df_Opp_Ag['DATE']<day) & (df_Opp_Ag['DATE']>=dayNback)]
+        #Need to fix the criteria here once date problem is fixed
+    	  #Calculate metrics for opposing team
+        Opp_3Pperc_Ag=df_Opp_Ag['3P'].sum()/df_Opp_Ag['3PA'].sum()
+        Opp_3PT_Ag=df_Opp_Ag['3P'].sum()
+        
+        
+    	  #################################################################################
+    	  #Go through each player
+        for i in df_Game['PLAYER FULL NAME']:
+            df_Player=df[df['PLAYER FULL NAME']==i]
+            actual=df_Player[df_Player['DATE']==day]['3P'].iloc[0] 
+            #Only want data before today to calculate historical statistics
+            df_Player=df_Player[df_Player['DATE']<day]
+            if df_Player.shape[0]>=N:
+                if sum(df_Player['3P'].tail(N))>threshold*N:
+                    #Calculate player statistics
+                    Player_3PT=(df_Player['3P'].tail(N)).sum()
+                    Player_3PT_median=(df_Player['3P'].tail(N)).median()
+                    Player_3PT_max=(df_Player['3P'].tail(N)).max()
+                    Player_3PT_min=(df_Player['3P'].tail(N)).min()
+                    Player_3PT_S=(df_Player['3P'].tail(N2)).sum()
+                    Player_3PTperc=(df_Player['3P'].tail(N)).sum()/(df_Player['3PA'].tail(N)).sum()                
+                    DaysOff=day-df_Player['DATE'].iloc[-1]				
+                    #Put result together with indicators
+                    df_temp=pd.DataFrame([actual,Player_3PT,Player_3PT_median,Player_3PT_max,Player_3PT_min,
+                                          Player_3PT_S,Player_3PTperc,Opp_3PT_Ag,Opp_3Pperc_Ag,DaysOff,place,i,day]).transpose()
+                    df_out=pd.concat([df_out, df_temp])
+    return df_out
+        
+
+# -------------------------------------------------------------------------------------------------------------------
+#os.chdir('C:\\Users\Kareem Kudus\Desktop\Python Stuff\Basketball')
+df_data = pd.DataFrame()    
+def f_gameID(x):  
+    return x[1]+x[4]+x[5] 
+###########################2016-2017###################################
+df_main = pd.read_pickle('C:/Users/syad/OneDrive/NBA Python/BigDataBall Data/2016-2017 NBA Player Boxscore.pkl')
+
+df_main['GameID']=df_main.apply(f_gameID, axis=1)
+df_main['DATE'] = pd.to_datetime(df_main['DATE'])
+# loop for every game
+uniqueGames = df_main['GameID'].unique()
+for i in uniqueGames:
+    df_result = pd.DataFrame(partTwo(str(i), df_main))
+    df_data = pd.concat([df_data, df_result])
+    print(i)
+
+###########################2017-2018###################################
+df_main = pd.read_pickle('C:/Users/syad/OneDrive/NBA Python/BigDataBall Data/2017-2018 NBA Player Boxscore.pkl')
+df_main['GameID']=df_main.apply(f_gameID, axis=1)
+df_main['DATE'] = pd.to_datetime(df_main['DATE'])
+# loop for every game
+uniqueGames = df_main['GameID'].unique()
+for i in uniqueGames:
+    df_result = pd.DataFrame(partTwo(str(i),df_main))
+    df_data = pd.concat([df_data, df_result])
+    print(i)
+    
+    
+
+    
+df_data.columns=['Actual','Player_3PT','Player_3PT_median','Player_3PT_max','Player_3PT_min',
+                 'Player_3PT_S','Player_3PTperc',
+                 'Opp_3PT_Ag','Opp_3Pperc_Ag',
+                 'DaysOff','place','player','day']
+# ------------------------
+
+
+def f_dates(x):  
+    return x[9].days
+df_data['DaysOff']=df_data.apply(f_dates, axis=1)
+
+def f_place(x): 
+    if x[10]=='R':
+        return 0
+    else:
+        return 1
+df_data['place']=df_data.apply(f_place, axis=1)
+
+df_data.to_pickle('3Pt dataframe N'+str(N)+'.pkl')
+
+
---
+++ Rebounds/model selection - regressor - rebounds.py
@@ -0,0 +1,177 @@
+import pandas as pd
+import sklearn as sk
+import numpy as np
+import os 
+
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+
+
+#os.chdir('Rebounds DataFrames')
+os.chdir('C:\\GitHub\\nba-python\\Rebounds\\Rebounds Dataframes')
+
+
+############################User defined stuff###############################
+
+N=5
+stat='RBS'
+historical='Player_Rebounds'
+
+
+#Portion of data for train, test and validation
+test_size = 0.175
+#ptrain=0.7
+#ptest=0.2
+#pval=0.1
+
+
+############################Import and sort data###############################
+dataFile=stat+' dataframe N'+str(N)+'.pkl'
+df = pd.read_pickle(dataFile)
+df = df.dropna()
+df = df.drop(['player', 'day'], axis=1)
+
+
+
+
+#Split and sort into train, test and validate based on proportions defined by user
+#num=df.shape[0]
+#dftrain=df.iloc[0:round(num*ptrain)]
+#dftest=df.iloc[round(num*ptrain)+1:round(num*(ptrain+ptest))]
+#dfvalidate=df.iloc[round(num*(ptrain+ptest))+1:num]
+
+
+dftrain, dftest = train_test_split(df, test_size=test_size, random_state=101)
+
+
+#################Format data for training and teting model#####################
+#Training data
+trainy = pd.to_numeric(dftrain['Actual'])
+trainx = (dftrain.drop(['Actual'], axis=1)).values
+#Test data
+testy = pd.to_numeric(dftest['Actual'])
+testx = dftest.drop(['Actual'], axis=1).values
+#Validation data
+#validatey = pd.to_numeric(dfvalidate['Actual'])
+#validatex = dfvalidate.drop(['Actual'], axis=1).values
+
+######################Try a simple linear regression########################
+model = LinearRegression()
+model.fit(trainx, trainy)
+#error1 = sk.metrics.log_loss(trainy,model.predict_proba(trainx))
+#error2 = sk.metrics.log_loss(testy,model.predict_proba(testx))
+error1 = sk.metrics.mean_squared_error(trainy, model.predict(trainx))
+error2 = sk.metrics.mean_squared_error(testy, model.predict(testx))
+print("Logistic regression results")
+print("Mean Squared Error: train then test")
+print(error1)
+print(error2)
+print(model.coef_)
+
+
+#########################Optimize Boosted Tree#################################
+#Initial guess for parameters
+opt_max_depth=2
+opt_min_split=5
+opt_min_samples_leaf=5
+opt_n_estimators=500
+opt_learning_rate=0.1
+
+#Optimize some parameters
+param_test1 = {'n_estimators':[100,200,400,800,1600,3200],'learning_rate':[0.001,0.003,0.01,0.03,0.1],'max_depth':[1,2,3,4,5]}
+#param_test1 = {'n_estimators':[100],'learning_rate':[0.001],'max_depth':[1]}
+
+gsearch1 = GridSearchCV(estimator = GradientBoostingRegressor(min_samples_split=opt_min_split,min_samples_leaf=opt_min_samples_leaf), iid=False, 
+                                                              param_grid = param_test1,scoring='neg_mean_squared_error',
+                                                              cv=5, verbose=0)
+gsearch1.fit(trainx, trainy)
+opt_n_estimators=gsearch1.best_params_['n_estimators']
+opt_learning_rate=gsearch1.best_params_['learning_rate']
+opt_max_depth=gsearch1.best_params_['max_depth']
+#gsearch1.grid_scores_
+
+#Optimize some parameters
+param_test2 = {'min_samples_leaf':[10,25,50,100], 'min_samples_split':[10,25,50,100]}
+#param_test2 = {'min_samples_leaf':[10], 'min_samples_split':[10]}
+
+gsearch2 = GridSearchCV(estimator = GradientBoostingRegressor(n_estimators=opt_n_estimators,max_depth=opt_max_depth,
+                                                              learning_rate=opt_learning_rate),iid=False,
+                                                              param_grid = param_test2,scoring='neg_mean_squared_error',
+                                                              cv=5, verbose=0)
+gsearch2.fit(trainx, trainy)
+opt_min_samples_leaf=gsearch2.best_params_['min_samples_leaf']
+opt_min_split=gsearch2.best_params_['min_samples_split']
+
+#Test out the optimized model
+model = GradientBoostingRegressor(n_estimators=opt_n_estimators,min_samples_split=opt_min_split,
+                                max_depth=opt_max_depth,learning_rate=opt_learning_rate,
+                                min_samples_leaf=opt_min_samples_leaf)
+model.fit(trainx, trainy)
+
+error3 = sk.metrics.mean_squared_error(trainy,model.predict(trainx))
+error4 = sk.metrics.mean_squared_error(testy,model.predict(testx))
+print("Optimized tree results")
+print(error3)
+print(error4)
+features=model.feature_importances_
+temp=dftest
+temp = temp[['Actual']]
+temp['predicted']=model.predict(testx)
+
+
+###########################See how a naive model does##########################
+dataFile=stat+' dataframe N'+str(N)+'.pkl'
+df = pd.read_pickle(dataFile)
+df = df.dropna()
+df=df[['Actual',historical]]
+
+#num=df.shape[0]
+#dftrain=df.iloc[0:round(num*ptrain)]
+#dftest=df.iloc[round(num*ptrain)+1:round(num*(ptrain+ptest))]
+#dfvalidate=df.iloc[round(num*(ptrain+ptest))+1:num]
+
+dftrain, dftest = train_test_split(df, test_size=test_size, random_state=101)
+trainy = pd.to_numeric(dftrain['Actual'])
+trainx = (dftrain.drop(['Actual'], axis=1)).values
+testy = pd.to_numeric(dftest['Actual'])
+testx = dftest.drop(['Actual'], axis=1).values
+
+#validatey = pd.to_numeric(dfvalidate['Actual'])
+#validatex = dfvalidate.drop(['Actual'], axis=1).values
+
+model = LogisticRegression()
+model.fit(trainx, trainy)
+error5 = sk.metrics.mean_squared_error(trainy,model.predict(trainx))
+error6 = sk.metrics.mean_squared_error(testy,model.predict(testx))
+print("Naive model results")
+print(error5)
+print(error6)
+print(model.coef_)
+temp['predictedNaive']=model.predict(testx)
+
+
+
+###########################Output the results##########################
+df_out_old = pd.read_csv(stat+' results.csv')
+
+df_out=pd.DataFrame([N,dataFile,opt_n_estimators,opt_min_split,opt_max_depth,
+        opt_learning_rate,opt_min_samples_leaf,error1,error2,error3,
+        error4,error5,error6,test_size]).transpose()
+
+df_out.columns=['N', 'dataFile','opt_n_estimators','opt_min_split','opt_max_depth',
+                'opt_learning_rate','opt_min_samples_leaf','Linear train','Linear test',
+                'Boost train','Boost test','Naive train','Naive test','test size']
+
+df_out = pd.concat([df_out_old, df_out])
+
+df_out.to_csv(stat+' results.csv',index=False)
+
+
+
+
+
+
+
