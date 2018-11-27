7a8eb3391d7f8227a5a110afebaef43850efee9b
kkudus <45290759+kkudus@users.noreply.github.com>
2018-11-26 20:46:45 -0500

    Update model selection - regressor - rebounds.py
    Updated code to store results of optimization and tests in xls file

--- Rebounds/model selection - regressor - rebounds.py
+++ Rebounds/model selection - regressor - rebounds.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sklearn as sk
 import numpy as np
 import os 
+import os.path
 
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
@@ -16,7 +17,7 @@ os.chdir('C:\\GitHub\\nba-python\\Rebounds\\Rebounds Dataframes')
 
 ############################User defined stuff###############################
 
-N=5
+N=10
 stat='RBS'
 historical='Player_Rebounds'
 
@@ -153,7 +154,9 @@ temp['predictedNaive']=model.predict(testx)
 
 
 ###########################Output the results##########################
-df_out_old = pd.read_csv(stat+' results.csv')
+if os.path.isfile(stat+' results.csv'):
+    df_out_old = pd.read_csv(stat+' results.csv')
+    df_out = pd.concat([df_out_old, df_out])
 
 df_out=pd.DataFrame([N,dataFile,opt_n_estimators,opt_min_split,opt_max_depth,
         opt_learning_rate,opt_min_samples_leaf,error1,error2,error3,
@@ -163,7 +166,8 @@ df_out.columns=['N', 'dataFile','opt_n_estimators','opt_min_split','opt_max_dept
                 'opt_learning_rate','opt_min_samples_leaf','Linear train','Linear test',
                 'Boost train','Boost test','Naive train','Naive test','test size']
 
-df_out = pd.concat([df_out_old, df_out])
+
+
 
 df_out.to_csv(stat+' results.csv',index=False)
 
