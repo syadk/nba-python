358fdc8c890751b67775d9cf12c2caf76c8c8518
syadk <41011700+syadk@users.noreply.github.com>
2020-01-14 21:36:49 -0800

    Update Merge Player Stats with Fantasy Stats.py
    2018-2019 starter column from string to int

--- Fantasy/Merging Player stats and FPS/Merge Player Stats with Fantasy Stats.py
+++ Fantasy/Merging Player stats and FPS/Merge Player Stats with Fantasy Stats.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import numpy as np
 
 
 
@@ -26,7 +27,37 @@ df2016_player.columns = ['DATASET', 'DATE',  'PLAYER',
        'DR', 'TOT', 'A', 'PF', 'ST', 'TO', 'BL', 'PTS']
 df2016_merged = df2016_player.merge(df2016_dfs, how='left', left_on=['DATE', 'PLAYER'],
                             right_on=['DATE', 'PLAYER'])
-del df2016_dfs, df2016_player
+
+#import team data to add indicator for starter
+df2016_team = pd.read_excel('2016-2017_NBA_Box_Score_Team-Stats.xlsx')
+df2016_team.drop(columns=['1Q', '2Q', '3Q', '4Q', 'OT1',
+       'OT2', 'OT3', 'OT4', 'F', 'MIN', 'FG', 'FGA', '3P', '3PA', 'FT', 'FTA',
+       'OR', 'DR', 'TOT', 'A', 'PF', 'ST', 'TO', 'TO TO', 'BL', 'PTS', 'POSS',
+       'PACE', 'OEFF', 'DEFF', 'REST DAYS', 'MAIN REF', 'CREW',
+       'OPENING ODDS', 'OPENING SPREAD', 'OPENING TOTAL', 'MOVEMENTS',
+       'CLOSING ODDS', 'MONEYLINE', 'HALFTIME', 'BOX SCORE', 'ODDS'], inplace=True)
+df2016_merged['STARTER (Y/N)'] = 0
+starter_list = []
+for i in range(df2016_merged.shape[0]):
+    player = df2016_merged['PLAYER'].iloc[i]
+    date = df2016_merged['DATE'].iloc[i]
+    team = df2016_merged['OWN TEAM'].iloc[i]
+    dftemp = df2016_team[df2016_team['DATE'] == date]
+    dftemp = dftemp[dftemp['TEAMS'] == team]
+    if player in dftemp.values:
+        starter_list.append(1)
+    else:
+        starter_list.append(0)
+    print(i)
+df2016_merged['STARTER (Y/N)'] = starter_list
+
+df2016_merged = df2016_merged[['DATASET', 'DATE', 'PLAYER', 'POSITION', 'OWN TEAM', 'OPPONENT TEAM',
+       'VENUE (R/H)', 'STARTER (Y/N)', 'MIN', 'FG', 'FGA', '3P', '3PA', 'FT',
+       'FTA', 'OR', 'DR', 'TOT', 'A', 'PF', 'ST', 'TO', 'BL', 'PTS',
+       'SAL - DRAFTKINGS', 'SAL - FANDUEL', 'FPS - DRAFTKINGS',
+       'FPS - FANDUEL']]
+
+del df2016_dfs, df2016_player, starter_list, df2016_team
 
 
 ##########################      2017-2018
@@ -51,7 +82,37 @@ df2017_player.columns = ['DATASET', 'DATE',  'PLAYER',
 
 df2017_merged = df2017_player.merge(df2017_dfs, how='left', left_on=['DATE', 'PLAYER'],
                             right_on=['DATE', 'PLAYER'])
-del df2017_dfs, df2017_player
+
+#import team data to add indicator for starter
+df2017_team = pd.read_excel('2017-2018_NBA_Box_Score_Team-Stats.xlsx')
+df2017_team.drop(columns=['1Q', '2Q', '3Q', '4Q', 'OT1',
+       'OT2', 'OT3', 'OT4', 'F', 'MIN', 'FG', 'FGA', '3P', '3PA', 'FT', 'FTA',
+       'OR', 'DR', 'TOT', 'A', 'PF', 'ST', 'TO', 'TO TO', 'BL', 'PTS', 'POSS',
+       'PACE', 'OEFF', 'DEFF', 'REST DAYS', 'MAIN REF', 'CREW',
+       'OPENING ODDS', 'OPENING SPREAD', 'OPENING TOTAL', 'MOVEMENTS',
+       'CLOSING ODDS', 'MONEYLINE', 'HALFTIME', 'BOX SCORE', 'ODDS'], inplace=True)
+df2017_merged['STARTER (Y/N)'] = 0
+starter_list = []
+for i in range(df2017_merged.shape[0]):
+    player = df2017_merged['PLAYER'].iloc[i]
+    date = df2017_merged['DATE'].iloc[i]
+    team = df2017_merged['OWN TEAM'].iloc[i]
+    dftemp = df2017_team[df2017_team['DATE'] == date]
+    dftemp = dftemp[dftemp['TEAMS'] == team]
+    if player in dftemp.values:
+        starter_list.append(1)
+    else:
+        starter_list.append(0)
+    print(i)
+df2017_merged['STARTER (Y/N)'] = starter_list
+
+df2017_merged = df2017_merged[['DATASET', 'DATE', 'PLAYER', 'POSITION', 'OWN TEAM', 'OPPONENT TEAM',
+       'VENUE (R/H)', 'STARTER (Y/N)', 'MIN', 'FG', 'FGA', '3P', '3PA', 'FT',
+       'FTA', 'OR', 'DR', 'TOT', 'A', 'PF', 'ST', 'TO', 'BL', 'PTS',
+       'SAL - DRAFTKINGS', 'SAL - FANDUEL', 'FPS - DRAFTKINGS',
+       'FPS - FANDUEL']]
+
+del df2017_dfs, df2017_player, starter_list, df2017_team
 
 ########################     2018-2019
 df2018_dfs = pd.read_excel('NBA-2018-2019-DFS-Dataset.xlsx', header=1)
@@ -73,12 +134,22 @@ df2018_player.columns = ['DATASET', 'GAME-ID', 'DATE', 'PLAYER-ID', 'PLAYER',
        'STARTER (Y/N)', 'MIN', 'FG', 'FGA', '3P', '3PA', 'FT', 'FTA', 'OR',
        'DR', 'TOT', 'A', 'PF', 'ST', 'TO', 'BL', 'PTS', 'USAGE RATE',
        'DAYS REST']
-df2018_player.drop(columns = ['GAME-ID', 'PLAYER-ID', 'STARTER (Y/N)', 'USAGE RATE', 'DAYS REST'], inplace=True)
+df2018_player.drop(columns = ['GAME-ID', 'PLAYER-ID', 'USAGE RATE', 'DAYS REST'], inplace=True)
 
 df2018_merged = df2018_player.merge(df2018_dfs, how='left', left_on=['DATE', 'PLAYER'],
                             right_on=['DATE', 'PLAYER'])
 
+
 del df2018_dfs, df2018_player
+
+#clean the starter column from string to int
+def string_int(x):
+    if (x=='Y'):
+        y=1
+    else:
+        y=0
+    return(y)
+df2018_merged['STARTER (Y/N)'] = df2018_merged['STARTER (Y/N)'].apply(string_int)
 
 ######################       save files
 df2016_merged.to_excel('NBA-2016-2017-Player-Boxscore-DFS_merged.xlsx')
